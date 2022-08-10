f205d572741be534bb2ad8d323f735abe266be85
Onam Kwon <kon6443@gmail.com>
2022-08-10 12:14:33 +0900

    first commit

---
+++ 1/main.js
@@ -0,0 +1,9 @@
+var nico = {
+    name: "nico"
+};
+var lynn = {
+    name: "lynn",
+    age: 20
+};
+console.log(nico);
+console.log(lynn);
---
+++ 1/main.ts
@@ -0,0 +1,16 @@
+type Player = {
+    name: String,
+    age?: number
+}
+
+const nico : Player = {
+    name: "nico"
+}
+
+const lynn: Player = {
+    name: "lynn",
+    age: 20
+}
+
+console.log(nico);
+console.log(lynn);

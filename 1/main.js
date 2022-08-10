f8767686545f277472602f2c2b91a7f9077d3b16
Onam Kwon <kon6443@gmail.com>
2022-08-10 16:38:11 +0900

    overloading

--- 1/main.js
+++ 1/main.js
@@ -7,3 +7,14 @@ var lynn = {
 };
 console.log(nico);
 console.log(lynn);
+var playerMaker = function (name) { return ({ name: name }); };
+// function playerMaker(name: Name) : Player { // telling that the return type is a Player object
+//     return {
+//         name: name
+//     }
+// }
+var five = playerMaker('five');
+five.age = 20;
+console.log(five);
+var add = function (a, b) { return a + b; };
+console.log(add(1, 2));
--- 1/main.ts
+++ 1/main.ts
@@ -1,6 +1,9 @@
+type Age = number;
+type Name = string;
+
 type Player = {
-    name: String,
-    age?: number
+    name: Name,
+    age?: Age // optional type declaratioin.
 }
 
 const nico : Player = {
@@ -14,3 +17,41 @@ const lynn: Player = {
 
 console.log(nico);
 console.log(lynn);
+
+const playerMaker = (name:Name) : Player => ({name});
+// function playerMaker(name: Name) : Player { // telling that the return type is a Player object
+//     return {
+//         name: name
+//     }
+// }
+
+let five = playerMaker('five');
+five.age = 20;
+console.log(five);
+
+// any: any type of type
+// undefined: declaration X, initialization X
+// null: declaration O, initializaton X
+
+type Add = (a: number, b: number) => number;
+const add:Add = (a, b) => a+b;
+console.log(add(1,2));
+
+
+// overloading
+type Config = {
+    path: string,
+    state: object
+}
+
+type Push = {
+    (path: string): void
+    (config: Config): void
+}
+
+const push: Push = (config) => { 
+    if(typeof config === "string") { console.log(config) }
+    else {
+        console.log(config.path, config.state)
+    }
+}
